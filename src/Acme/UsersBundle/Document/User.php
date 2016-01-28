a7f48d6f14138460251260a547a58a5c6aaaea95
Pavel Gordeev <mitridat2000@gmail.com>
2016-01-29 01:03:52 +0300

    add bunle and generate get set

---
+++ src/Acme/UsersBundle/Document/User.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Acme\UsersBundle\Document;
+
+class User
+{
+    protected $name;
+
+    protected $password;
+    
+    protected $role;
+    /**
+     * @var MongoId $id
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return id $id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return self
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string $name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return self
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string $password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     * @return self
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string $role
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+}
