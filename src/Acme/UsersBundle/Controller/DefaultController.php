25f1ad1b3e795d1c208ebf81776742c42e834d0b
Pavel Gordeev <mitridat2000@gmail.com>
2016-01-30 02:43:43 +0300

    add show action

--- src/Acme/UsersBundle/Controller/DefaultController.php
+++ src/Acme/UsersBundle/Controller/DefaultController.php
@@ -6,52 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Acme\UsersBundle\Document\User;
 use Acme\UsersBundle\Document\Group;
 use Symfony\Component\HttpFoundation\Response;
-
 use Acme\UsersBundle\Form\Type\RegistrationType;
 use Acme\UsersBundle\Form\Type\UserType;
 use Acme\UsersBundle\Form\Model\Registration;
-
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('AcmeUsersBundle:Default:index.html.twig');
+class DefaultController extends Controller {
+
+    public function indexAction() {
+        return $this->redirectToRoute('acme_users_show');
     }
-    
-    public function create2Action()
-    {
+
+    public function create2Action() {
         $group = $this->get('doctrine_mongodb')
-            ->getManager()
-            ->getRepository('AcmeUsersBundle:Group')
-            ->getDefaultGroup();
-        
-        
+                ->getManager()
+                ->getRepository('AcmeUsersBundle:Group')
+                ->getDefaultGroup();
+
+
         $user = new User();
         $user->setName('user2');
         $user->setPassword('19.99');
-        
-       
+
+
 
         $dm = $this->get('doctrine.odm.mongodb.document_manager');
         $dm->persist($user);
         $dm->flush();
 
-        return new Response('Created product id '.$user->getId());
+        return new Response('Created product id ' . $user->getId());
     }
-    
-    
-    public function registerAction()
-    {
+
+    public function registerAction() {
         $form = $this->createForm(new RegistrationType(), new Registration());
         return $this->render('AcmeUsersBundle:Users:register.html.twig', array('form' => $form->createView()));
     }
-    
-    public function createAction()
-    {   
+
+    public function createAction() {
         $dm = $this->get('doctrine.odm.mongodb.document_manager');
 
         $user = new User();
@@ -67,10 +60,18 @@ class DefaultController extends Controller
             $dm->persist($registration->getUser());
             $dm->flush();
 
-            return $this->redirectToRoute('acme_users_register');
+            return $this->redirectToRoute('acme_users_show');
         }
 
         return $this->render('AcmeUsersBundle:Users:register.html.twig', array('form' => $form->createView()));
     }
-    
+
+    public function showAction() {
+        $users = $this->get('doctrine_mongodb')
+                ->getManager()
+                ->getRepository('AcmeUsersBundle:User')
+                ->findAllOrderedByName();
+        return $this->render('AcmeUsersBundle:Users:show.html.twig', array('users' => $users));
+    }
+
 }
